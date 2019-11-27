3eea351a739f459222a1a4bb55518e2561755157
Sly_tom_cat <slytomcat@mail.ru>
2019-11-27 19:29:51 +0300

    token creator service added

--- URLshortener.go
+++ URLshortener.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-var homePage = string(`
+var homePage = []byte(`
 <html>
 	<body>
 	   Home page of URLshortener
@@ -16,34 +19,87 @@ var homePage = string(`
 
 var tokenDB *TokenDB
 
-// Home shows home page
-func Home(w http.ResponseWriter) {
-	fmt.Fprint(w, homePage)
+/* test:
+curl -i -v http://localhost:8080/
+*/
+
+// Home shows simple home page
+// It can be used to check the service health.
+func home(w http.ResponseWriter) {
+	if tokenDB.DB.Ping() != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(homePage)
 }
+
+/* test:
+curl -i -v http://localhost:8080/<token>
+*/
 
 func redirect(w http.ResponseWriter, r *http.Request, sToken string) {
 	longURL, err := tokenDB.Get(sToken)
 	if err != nil {
-		w.WriteHeader(404)
+		http.NotFound(w, r)
 		fmt.Printf("URL fror token '%s' was not found\n", sToken)
 		return
 	}
 	fmt.Println("Redirest to ", longURL)
-	w.Header()["Location"] = []string{longURL}
-	w.WriteHeader(301)
+	http.Redirect(w, r, longURL, 301)
+}
+
+/* test:
+curl -v POST -H "Content-Type: application/json" -d '{"url":"https://www.w3schools.com/html/html_forms.asp","exp":"10"}' http://localhost:8080/token
+*/
+
+func getNewToken(w http.ResponseWriter, r *http.Request) {
+	// ????: check some authorisation???
+
+	var params struct {
+		URL string `json:"url"`
+		Exp int    `json:"exp,string,omitempty"`
+	}
+
+	buf := make([]byte, r.ContentLength)
+
+	_, err := r.Body.Read(buf)
+	if err != nil && !errors.Is(err, io.EOF) {
+		fmt.Printf("request body reading error: %v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	err = json.Unmarshal(buf, &params)
+	if err != nil || params.URL == "" {
+		fmt.Printf("bad request")
+		w.WriteHeader(400)
+		return
+	}
+
+	if params.Exp == 0 {
+		params.Exp = 1 // TODO: should be default expiration from config
+	}
+	sToken, err := tokenDB.New(params.URL, params.Exp)
+
+	if err != nil {
+		fmt.Printf("new token creation error: %v\n", err)
+		w.WriteHeader(504)
+		return
+	}
+	fmt.Printf("[ %s | %s | %d ]\n", sToken, params.URL, params.Exp)
+	w.Write([]byte(sToken))
 }
 
 func myMUX(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
-	fmt.Println("Path:", path)
 	switch path {
 	case "/":
 		fmt.Println("Home")
-		Home(w)
+		home(w)
 	case "/token":
 		fmt.Println("Request for token")
-		// todo
-	case "/favicon.ico":
+		getNewToken(w, r)
+	case "/favicon.ico": // I have no idea why the chromium make such requests together with request for redirect
 		return
 	default:
 		fmt.Println("Request for redirect")
@@ -52,13 +108,17 @@ func myMUX(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	var err error
+
 	tokenDB, err = TokenDBNew()
 	if err != nil {
 		panic(err)
 	}
 
 	http.HandleFunc("/", myMUX)
+
+	// TODO: read host:port from config file
 	fmt.Println("starting server at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
