0fc1360a7d0f1da3421fd32f71dcf46bffb00e13
Sly_tom_cat <slytomcat@mail.ru>
2019-12-05 19:27:46 +0300

    big code review

--- ShortToken_test.go
+++ ShortToken_test.go
@@ -1,31 +1,36 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 // try to create new token from debugging source
-func Test05ShortTokenNewFake(t *testing.T) {
+func Test05NewShortTokenFake(t *testing.T) {
 	DEBUG = true
-	tc, err := ShortTokenNew()
+	tc, err := NewShortToken()
 	if err != nil {
 		t.Error("error of ShortToken creation from debug source:", err)
 	}
 
-	if tc != "______" {
+	if tc != strings.Repeat("_", tokenLenS) {
 		t.Error("wrong token BASE64 representation")
 	}
 }
 
 // try to make two tokens from random source and compare them
-func Test07ShortTokenNewReal(t *testing.T) {
+func Test07NewShortTokenReal(t *testing.T) {
 	DEBUG = false
-	tc, err := ShortTokenNew()
+	tc, err := NewShortToken()
 	if err != nil {
 		t.Error("error of ShortToken creation from random:", err)
 	}
 
-	tc1, _ := ShortTokenNew()
+	tc1, _ := NewShortToken()
+
+	if len(tc) != len(tc1) || len(tc) != tokenLenS {
+		t.Error("wrong token length")
+	}
 
 	if tc == tc1 {
 		t.Error("2 sequential token are equal by BASE64")
