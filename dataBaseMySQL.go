97836aee72aef2a31284bbca6f516bd7d1b2ac40
Sly_tom_cat <slytomcat@mail.ru>
2020-01-09 16:33:48 +0300

    rename

---
+++ dataBaseMySQL.go
@@ -0,0 +1,189 @@
+package main
+
+// TokenDB provides parallel execution safe methods to store, update and
+// retrieve tokens from data base.
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// TokenDBM is a structure to handle the DB token operations
+type TokenDBM struct {
+	db *sql.DB // database connection
+}
+
+// TokenDBNewM - creates new TokenDB struct and connect to mysql server
+func TokenDBNewM() (*TokenDBM, error) {
+
+	db, err := sql.Open("mysql", CONFIG.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	// set the connection pool size
+	db.SetMaxOpenConns(CONFIG.MaxOpenConns)
+
+	// Check the connection
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenDBM{db}, nil
+}
+
+// New returns new token for given long URL and store the token expiration period (in days)
+func (t *TokenDBM) New(longURL string, expiration int) (string, error) {
+	var err error
+	// token of saved long URL
+	sToken := ""
+
+	// Try several times to create new token and insert it into DB table.
+	// The token field is unique in DB so it's not possible to insert the same token twice.
+	// But if the token is already expired then try to update it (set new url and expiration).
+
+	// Using 10 attempts to insert/update token dramatically increases maximum amount of
+	// used tokens since :
+	// probability of the failure of n attempts = (probability of failure of single attempt)^n.
+	attempt := 0
+	for ; attempt < 10; attempt++ {
+
+		// get new token
+		sToken, err = NewShortToken()
+		if err != nil {
+			return "", err
+		}
+
+		// begin transaction
+		tran, err := t.db.Begin()
+		if err != nil {
+			return "", fmt.Errorf("can't create transaction: %w", err)
+		}
+
+		// try to store new token
+		_, err = tran.Exec(
+			"INSERT INTO urls (`token`, `url`, `exp`) VALUES (?, ?, ?)",
+			sToken,
+			longURL,
+			expiration,
+		)
+		if err == nil {
+			// the token is successfully inserted
+			tran.Commit()
+			break
+		}
+
+		// handle error if it is not Duplicate entry error
+		if !strings.Contains(err.Error(), "Duplicate entry") {
+			tran.Rollback()
+			return "", fmt.Errorf("can't insert token: %w", err)
+		}
+
+		// the token is already exists: try to update the token if it is expired
+		result, err := tran.Exec("UPDATE `urls` SET `url`=?, `exp`=? WHERE `token` = ? and DATE_ADD(`ts`, INTERVAL `exp` DAY) < NOW()",
+			longURL,
+			expiration,
+			sToken,
+		)
+		if err != nil {
+			tran.Rollback()
+			return "", fmt.Errorf("can't update token: %w", err)
+		}
+
+		// check affected rows
+		affected, err := result.RowsAffected()
+		if err == nil && affected == 1 {
+			// token successfully updated
+			tran.Commit()
+			break
+		}
+		tran.Rollback()
+		if err != nil {
+			return "", fmt.Errorf("can't get affected rows: %w", err)
+		}
+		// token is not insrted and not updated
+		// reset bad token
+		sToken = ""
+	}
+
+	if sToken == "" {
+		// if we can't insert/update random token for several tries, then
+		// it seems that all tokens are busy
+		return "", fmt.Errorf("can't store a new token")
+	}
+
+	// log the warning when the saving of new token took too many attempts
+	if attempt > 6 {
+		log.Printf("WARNING: It took %d attempts for saving the new token\n", attempt)
+	}
+
+	// return the successfully inserted or updated token
+	return sToken, nil
+}
+
+// Get returns long url for given token
+func (t *TokenDBM) Get(sToken string) (string, error) {
+	if len(sToken) != tokenLenS {
+		return "", errors.New("wrong token length")
+	}
+
+	// get the url by token checking expiratinon
+	row := t.db.QueryRow("SELECT url FROM urls WHERE token = ? and DATE_ADD(`ts`, INTERVAL `exp` DAY) > NOW()", sToken)
+
+	url := ""
+	err := row.Scan(&url)
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
+}
+
+// Expire - set new expiration on the token
+// Use zero or negative exp value to expire token
+func (t *TokenDBM) Expire(sToken string, exp int) error {
+
+	// begin transaction
+	tran, err := t.db.Begin()
+	if err != nil {
+		return fmt.Errorf("can't create transaction: %w", err)
+	}
+
+	// try update token
+	result, err := tran.Exec("UPDATE `urls` SET `exp`=? WHERE `token` = ? ", exp, sToken)
+	if err != nil {
+		tran.Rollback()
+		return fmt.Errorf("can't update token: %w", err)
+	}
+
+	// check affected rows
+	if affected, err := result.RowsAffected(); err != nil || affected != 1 {
+		tran.Rollback()
+		if err != nil {
+			return fmt.Errorf("can't get affected rows: %w", err)
+		}
+		return errors.New("token is not found")
+	}
+
+	// commit transaction
+	tran.Commit()
+	return nil
+}
+
+// Delete deletes specified token. It requered mostly for tests
+func (t *TokenDBM) Delete(sToken string) error {
+	tx, _ := t.db.Begin()
+	_, err := tx.Exec("DELETE FROM urls WHERE token=?", DEBUGToken)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
