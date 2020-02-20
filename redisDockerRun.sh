d5e1cf5dbce61b66dc38b933edc375ed1e4c687b
Sly_tom_cat <slytomcat@mail.ru>
2020-02-20 22:36:22 +0300

    Add security note

--- redisDockerRun.sh
+++ redisDockerRun.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#### IMPORTANT NOTE ####
+## Suggested method must be used only for test purposes!
+## Never pass the password through the command line parameter in production.
+## The full command line (with password) can be obtained via `ps` comand by ANY OS user!
+########################
+
 docker run --name redis -d -p 6379:6379  redis:5.0.7-alpine redis-server --requirepass "some very long password that is provided through ConnectOptions.Password configuration value"
